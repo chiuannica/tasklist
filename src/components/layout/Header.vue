ee8cb4d2a2b945bb98bbbb4d18c9b2afdd4a3974
chiuannica <chiuannica@gmail.com>
2019-04-21 12:33:08 -0400

    the bare minimum

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        <h1>Todo List</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background: pink;
+        color: white;
+    }
+</style>
